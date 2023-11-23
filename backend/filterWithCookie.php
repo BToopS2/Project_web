d298a7f5b0a5a6ed2e865b1cec0187ba550a6e46
hieuhh <hhhieu02@gmail.com>
2023-11-23 21:21:07 +0700

    dd

--- backend/filterWithCookie.php
+++ backend/filterWithCookie.php
@@ -3,12 +3,12 @@
     $checkCookie = Auth::loginWithCookie();
     if($checkCookie != null){
         if($checkCookie['role']==1){
-            echo '<a href="admin/production/shoe.php">TRUY CẬP TRANG ADMIN</a>';
+            echo '<a style="color:red" href="admin/production/shoe.php">TRUY CẬP TRANG ADMIN</a>';
         }
         echo '<a href="#">'.$checkCookie['fullname'].'</a>
         <a href="/shoe/backend/logoutCookie.php">Đăng Xuất</a>';
     }
     else{
-        echo '<a href="auth/login/index.php">Login & Regiser</a>';
+        echo '<a href="auth/login/index.php">Đăng ký & Đăng nhập</a>';
     }
 ?>
--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 $server = "localhost:3306";
 $user="root";
 $pass="";
-$database="shoe";
+$database="husstoredata";
 $conn=mysqli_connect($server,$user,$pass,$database);
 mysqli_query($conn,'set names "utf8"');
 ?>
