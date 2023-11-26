0089cf24a5aebc958d4409992aa1e1dcf23f1376
nguyenanhtuan0502 <147410342+nguyenanhtuan0502@users.noreply.github.com>
2023-11-26 10:25:11 -0500

    Add files via upload

---
+++ db/shoe2.sql
@@ -0,0 +1,306 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th10 26, 2023 lúc 04:40 AM
+-- Phiên bản máy phục vụ: 10.4.28-MariaDB
+-- Phiên bản PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `shoe2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `shoe_id` int(11) NOT NULL,
+  `shoe_color` varchar(255) NOT NULL,
+  `shoe_size` int(11) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `cart`
+--
+
+INSERT INTO `cart` (`id`, `user_id`, `shoe_id`, `shoe_color`, `shoe_size`, `status`) VALUES
+(6, 2, 25, 'hồng', 23, 1),
+(7, 1, 28, 'xanh', 32, 2),
+(8, 19, 29, 'xanh', 32, 3),
+(9, 19, 28, 'xanh', 32, 3),
+(10, 1, 28, 'xanh', 32, 3),
+(11, 19, 29, 'xanh', 32, 1),
+(12, 1, 29, 'xanh', 32, 3),
+(13, 20, 30, 'red', 231, 2),
+(14, 20, 29, 'xanh', 32, 2),
+(15, 20, 30, 'red', 231, 2),
+(16, 20, 29, 'xanh', 32, 2),
+(17, 20, 30, 'red', 231, 2),
+(18, 20, 30, 'red', 231, 2),
+(19, 20, 29, 'xanh', 32, 2),
+(20, 20, 28, 'xanh', 32, 3),
+(21, 20, 29, 'xanh', 32, 2),
+(22, 20, 30, 'xanh', 32, 2),
+(23, 20, 30, 'red', 231, 2),
+(24, 20, 29, 'xanh', 32, 2),
+(25, 20, 28, 'xanh', 32, 2),
+(26, 20, 30, 'red', 231, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `category`
+--
+
+INSERT INTO `category` (`id`, `name`) VALUES
+(1, 'men'),
+(2, 'women'),
+(3, 'kids'),
+(4, 'nike'),
+(5, 'adidas');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `cart_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `orders`
+--
+
+INSERT INTO `orders` (`id`, `cart_id`, `quantity`, `date`) VALUES
+(1, 1, 0, '2023-11-22'),
+(2, 2, 0, '2023-11-22'),
+(4, 8, 0, '2023-11-22'),
+(5, 9, 0, '2023-11-22'),
+(6, 10, 0, '2023-11-22'),
+(7, 12, 0, '2023-11-24'),
+(14, 19, 0, '2023-11-24'),
+(15, 20, 0, '2023-11-24'),
+(16, 21, 0, '2023-11-24'),
+(17, 22, 0, '2023-11-24'),
+(18, 23, 0, '2023-11-24'),
+(19, 24, 0, '2023-11-24'),
+(20, 25, 0, '2023-11-24'),
+(21, 26, 0, '2023-11-24');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `shoe`
+--
+
+CREATE TABLE `shoe` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` bigint(11) NOT NULL,
+  `sale` int(11) NOT NULL,
+  `size` varchar(255) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `color` varchar(255) NOT NULL,
+  `review` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `shoe`
+--
+
+INSERT INTO `shoe` (`id`, `name`, `price`, `sale`, `size`, `category_id`, `color`, `review`) VALUES
+(28, 'Nike air', 2000, 15, '32', 4, 'xanh, đỏ, vàng', 'Giày đẹp'),
+(29, 'Jordan', 100000, 50, '32,42,33', 4, 'xanh, đỏ, vàng', 'ddd'),
+(30, 'sssddd', 322, 23, '231', 2, 'red', 'ssad');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `shoe_image`
+--
+
+CREATE TABLE `shoe_image` (
+  `id` int(11) NOT NULL,
+  `shoe_id` int(11) NOT NULL,
+  `link_image` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `shoe_image`
+--
+
+INSERT INTO `shoe_image` (`id`, `shoe_id`, `link_image`) VALUES
+(1, 16, 'imageShoe/73039743_457799481516743_2944413433908428800_n.jpg'),
+(2, 16, 'imageShoe/74589546_545009112982009_579124783078178816_n.jpg'),
+(3, 16, 'imageShoe/146972117_2849928912001221_7521882077628797924_n.jpg'),
+(4, 15, 'imageShoe/278436512_1056949608191262_6272145579401916967_n.jpg'),
+(5, 15, 'imageShoe/278713399_1987770381397641_3834876700356754447_n.jpg'),
+(6, 15, 'imageShoe/ao.jpg'),
+(7, 19, 'imageShoe/167460640_548361456141577_4021222783376699911_n (2).jpg'),
+(8, 19, 'imageShoe/167460640_548361456141577_4021222783376699911_n.jpg'),
+(9, 19, 'imageShoe/186990832_2912094862453743_3770370240088288146_n.jpg'),
+(10, 19, 'imageShoe/213649488_187668229973939_9155369604254683281_n.jpg'),
+(21, 21, 'imageShoe/73039743_457799481516743_2944413433908428800_n.jpg'),
+(24, 24, 'imageShoe/146972117_2849928912001221_7521882077628797924_n.jpg'),
+(25, 24, 'imageShoe/ao.jpg'),
+(26, 23, 'imageShoe/73039743_457799481516743_2944413433908428800_n.jpg'),
+(27, 23, 'imageShoe/74589546_545009112982009_579124783078178816_n.jpg'),
+(29, 22, 'imageShoe/cogiaoman.png'),
+(30, 25, 'imageShoe/SharedScreenshot3.jpg'),
+(31, 25, 'imageShoe/SharedScreenshot4.jpg'),
+(32, 25, 'imageShoe/SharedScreenshot5.jpg'),
+(33, 26, 'imageShoe/SharedScreenshot.jpg'),
+(34, 26, 'imageShoe/SharedScreenshot2.jpg'),
+(35, 27, 'imageShoe/245073628_3091058767796051_5731964485928323993_n.jpg'),
+(36, 27, 'imageShoe/245495174_2898697773716058_6691009756593589867_n.png'),
+(37, 27, 'imageShoe/245642692_415139920308073_6895757974366559092_n.png'),
+(38, 28, 'imageShoe/14-8.jpg'),
+(39, 28, 'imageShoe/14-8.jpg'),
+(40, 29, 'imageShoe/hinh-nen-may-tinh-5k-songoku-sieu-xayda-1.jpg'),
+(41, 30, 'imageShoe/14-8.jpg'),
+(42, 30, 'imageShoe/hinh-nen-may-tinh-5k-songoku-sieu-xayda-1.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `dob` date NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `role` tinyint(1) NOT NULL DEFAULT 0,
+  `gender` tinyint(1) NOT NULL DEFAULT 0,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `fullname`, `dob`, `address`, `role`, `gender`, `email`, `phone`) VALUES
+(1, 'admin@gmail.com', '25f9e794323b453885f5181f1b624d0b', 'HoangHuuHieu', '2023-11-09', 'Vietnam', 1, 1, 'hieu@gmail.com', '2121321321'),
+(2, 'hieu@gmail.com', '25f9e794323b453885f5181f1b624d0b', 'Hieuhh', '2023-10-30', 'vietnam', 0, 0, 'g@gmail.com', '2132132133'),
+(19, 'Hieuhoang', 'e807f1fcf82d132f9bb018ca6738a19f', 'HieuHoang', '2023-11-18', 'Vietnam', 0, 1, 'hieu02@gmail.com', '3123212312'),
+(20, 'tuan0302', '25f9e794323b453885f5181f1b624d0b', 'Tuấn Nguyễn Anh', '2023-11-03', 'Ngõ 204/3 Phố Hồng Mai Quận Hai Bà Trưng Hà Nội', 0, 1, 'nguyenatuan0302@gmail.com', '0876687256');
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `shoe`
+--
+ALTER TABLE `shoe`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `shoe_image`
+--
+ALTER TABLE `shoe_image`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT cho bảng `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT cho bảng `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT cho bảng `shoe`
+--
+ALTER TABLE `shoe`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT cho bảng `shoe_image`
+--
+ALTER TABLE `shoe_image`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+
+--
+-- AUTO_INCREMENT cho bảng `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
