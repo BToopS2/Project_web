b890735f873df83fc4154318ec306a8ea3a5ab17
nguyenanhtuan0502 <147410342+nguyenanhtuan0502@users.noreply.github.com>
2023-11-26 10:26:58 -0500

    Add files via upload

--- repository/orderRepository.php
+++ repository/orderRepository.php
@@ -11,6 +11,19 @@
             $sql = "select u.*,c.*,o.*,s.*, o.id as order_id, c.id as cart_id from user u join cart c on u.id=c.user_id join orders o on o.cart_id = c.id join shoe s on s.id = c.shoe_id"; 
             return mysqli_query($conn,$sql);
         }
+        public function getAll2($userName){
+            global $conn;
+            $escapedUserName = mysqli_real_escape_string($conn, $userName); // Bảo vệ khỏi SQL injection
+        
+            $sql = "SELECT u.*, c.*, o.*, s.*, o.id AS order_id, c.id AS cart_id
+                    FROM user u
+                    JOIN cart c ON u.id = c.user_id
+                    JOIN orders o ON o.cart_id = c.id
+                    JOIN shoe s ON s.id = c.shoe_id
+                    WHERE u.fullname = '$escapedUserName'";  // Sử dụng tên người dùng để lọc
+            return mysqli_query($conn, $sql);
+        }
+        
         public function deleteById($id){
             global $conn;
             $sql = "delete from orders where id=$id"; 
